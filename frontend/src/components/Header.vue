ddcb5e8c730d5b3d42771c5fd7fb1d86574dd052
Ben Ward <benward83@gmail.com>
2020-10-07 17:52:39 +0200

    Basic layout of Home page added

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="header">
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4">Julia's Orgo App</h1>
+        <p class="lead">Welcome to the Orgo App</p>
+      </div>
+    </div>
+  </div>
+</template>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,85 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <div class="card-deck">
+      <div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">Ingredients</h5>
+          <img class="card-img-top" src="../assets/Ingredients.jpg">
+          <p class="card-text"></p>
+          <a href="#" class="card-link">Card link</a>
+          <a href="#" class="card-link">Another link</a>
+        </div>
+      </div>
+      <div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">Calendar</h5>
+          <img class="card-img-top" src="../assets/Calendar.jpg">
+          <p class="card-text"></p>
+          <a href="#" class="card-link">Card link</a>
+          <a href="#" class="card-link">Another link</a>
+        </div>
+      </div>
+      <div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">Shopping List</h5>
+          <img class="card-img-top" src="../assets/Shopping.jpg">
+          <p class="card-text"></p>
+          <a href="#" class="card-link">Card link</a>
+          <a href="#" class="card-link">Another link</a>
+        </div>
+      </div>
+      <div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">Shopping List</h5>
+          <img class="card-img-top" src="../assets/Recipes.jpg">
+          <p class="card-text"></p>
+          <a href="#" class="card-link">Card link</a>
+          <a href="#" class="card-link">Another link</a>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+
+import Header from '@/components/Header.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+    Header,
   },
+
 };
 </script>
+
+<style>
+.card-deck {
+  margin-bottom: 30px;
+}
+
+.card {
+   color: whitesmoke
+
+}
+
+.card-title {
+  color: black;
+  text-decoration-color: black;
+
+}
+
+.card-text {
+  color: black;
+  text-decoration-color: black;
+
+}
+
+.card-img-top {
+    width: 100%;
+    height: 15vw;
+    object-fit: cover;
+}
+</style>
