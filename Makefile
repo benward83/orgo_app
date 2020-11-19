fd469cffd0121b75360cbec06971ad20cb6dac28
Ben Ward <benward83@gmail.com>
2020-11-19 18:59:24 +0100

    [webspicy] I like it when a plan comes together.

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 SHELL=/bin/bash -o pipefail
-COMPONENTS := api frontend dbagent gateway
+COMPONENTS := api frontend dbagent gateway webspicy
 UP_COMPONENTS := gateway dbagent
 
 ################################################################################
@@ -215,5 +215,5 @@ frontend/Dockerfile.builder.built: frontend/Dockerfile.builder frontend/package.
 	docker build frontend ${frontend_BUILD_ARGS} -t orgo/frontend-builder -f frontend/Dockerfile.builder | tee frontend/Dockerfile.builder.log
 	touch frontend/Dockerfile.builder.built
 
-# api.test: webspicy.on
-# 	docker-compose exec -T webspicy bundle exec rake test
+api.test: webspicy.on
+	docker-compose run -T webspicy webspicy config.rb
--- api/src/routes/ingredients.js
+++ api/src/routes/ingredients.js
@@ -18,10 +18,14 @@ router.get('/', (req, res) => {
 // Get an ingredient with name and id
 
 router.get('/:id', (req, res) => {
-  db.select('name', 'id')
+  db.select('*')
     .from('ingredients')
+    .where('id', req.params.id)
     .then(result => {
-      res.json(result);
+      if (result.length) {
+        return res.json(result[0]);
+      }
+      res.json(404, { error: 'Ingredient not found' });
     })
     .catch(err => res.send(500, err))
 });
@@ -36,22 +40,39 @@ router.post('/', (req, res) => {
     .returning(['name', 'id'])
     .insert(newIngredient)
     .then(result => {
-      res.json(result);
+      return res.json(result[0]);
     })
     .catch(err => res.send(500, err))
 });
 
+// Update an ingredient
 
+router.patch('/:id', (req, res) => {
+  db('ingredients')
+    .returning(['*'])
+    .where('id', req.params.id)
+    .update(req.body)
+    .then(result => {
+      if (result.length) {
+        return res.json(result[0]);
+      }
+      return res.json(404, { error: 'Ingredient not found' });
+    })
+    .catch(err => res.send(500, err))
+});
 
 // Delete an ingredient
 
 router.delete('/:id', (req, res) => {
   db('ingredients')
-    .returning(['name', 'id'])
+    .returning(['*'])
     .where('id', req.params.id)
     .del()
     .then(result => {
-      res.json(result);
+      if (result.length) {
+        return res.json(result[0]);
+      }
+      return res.json(404, { error: 'Ingredient not found' });
     })
     .catch(err => res.send(500, err))
 });
