0209ffdb1aca91d276669e0462978e3903cfb8c9
Ben Ward <benward83@gmail.com>
2021-03-16 18:54:19 +0100

    [Frontend] Starting logic for AddRecipeForm

--- frontend/src/components/AddRecipeForm.vue
+++ frontend/src/components/AddRecipeForm.vue
@@ -28,35 +28,50 @@
           ></b-form-input>
         </div>
 
+            <!-- Dropdown -->
+
+        <div class="form-dropdown mt-3">
+          <b-form-select v-model="meatSelected" :meatOptions="meatOptions"></b-form-select>
+        </div>
+        <div class="form-dropdown mt-3">
+          <b-form-select v-model="fishSelected" :fishOptions="fishOptions"></b-form-select>
+        </div>
+
           <!-- Tags -->
 
         <div class="recipe-form-tags">
-          <h6>Add a tag</h6>
+          <h6>Add tags...</h6>
           <b-form-tags input-id="tags-basic" v-model="tags"></b-form-tags>
         </div>
 
         <!-- Text area -->
+        <div>
+          <h6>Please list the ingredients</h6>
+            <b-form-textarea
+              id="ingredient-textarea"
+              v-model="ingredients"
+              placeholder="Ingredient list..."
+              rows="3"
+              max-rows="6"
+            ></b-form-textarea>
+        </div>
 
         <div>
           <h6>Please leave a description of the recipe</h6>
             <b-form-textarea
-              id="textarea"
+              id="description-textarea"
               v-model="description"
-              placeholder="Enter something..."
+              placeholder="description..."
               rows="3"
               max-rows="6"
             ></b-form-textarea>
-            <pre class="mt-3 mb-0">{{ text }}</pre>
         </div>
 
           <!-- Btns -->
 
-        <div class="form-dropdown mt-3">
-          <b-form-select v-model="selected" :options="options"></b-form-select>
-        </div>
         <div class="rating mt-3">
           <h6 class="form-text">Please rate this recipe</h6>
-          <b-form-rating v-model="value" variant="warning" class="mb-2">
+          <b-form-rating v-model="recipeRating" variant="warning" class="mb-2">
           </b-form-rating>
         </div>
         <div class="form-btns mt-3">
@@ -77,8 +92,27 @@ export default {
   components: {},
   data() {
     return {
+      name: '',
+      time: '',
+      servings: '',
       tags: [],
+      ingredients: '',
       description: '',
+      meatSelected: null,
+      meatOptions: [
+        { value: null, text: 'Please select cut of meat' },
+        { value: 'a', text: 'This is First option' },
+        { value: 'b', text: 'Selected Option' },
+        { value: { C: '3PO' }, text: 'This is an option with object value' },
+      ],
+      fishSelected: null,
+      fishOptions: [
+        { value: null, text: 'Please select type of fish' },
+        { value: 'a', text: 'This is First option' },
+        { value: 'b', text: 'Selected Option' },
+        { value: { C: '3PO' }, text: 'This is an option with object value' },
+      ],
+      recipeRating: '',
     };
   },
 };
